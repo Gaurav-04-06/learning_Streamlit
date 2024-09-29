a9fb70a320e019cfd4249a77112543ffc16aa2af
Gaurav_Kohli <124597155+Gaurav-04-06@users.noreply.github.com>
2024-09-29 21:55:57 +0530

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ with st.sidebar:
   input_df = pd.DataFrame(data)
 
 input_penguins = pd.concat([input_df , X] , axis = 0)
+
+# Display the input data
+st.write("User Input Features")
+st.write(input_df)
