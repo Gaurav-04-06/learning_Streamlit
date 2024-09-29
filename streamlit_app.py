783ccf2af66a26120e9fa48aba8fcf3775a67430
Gaurav_Kohli <124597155+Gaurav-04-06@users.noreply.github.com>
2024-09-29 19:41:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("🤖Machine Learning App")
 
 st.info("Basic tutorial for learning streamlit with machine learning")
 
-with st.expander('Data):
+with st.expander('Data'):
   st.write("Raw Data")   
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
